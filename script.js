3a8a6ab92ab640650309421c07a05d52446bf09b
Carlos Henrique <carlosgracom389@gmail.com>
2026-06-17 22:59:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,20 +16,38 @@ let turnos = 1;
 
 // Baralho
 const baralho = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const zap = 0;
-const coringa = 0;
-const bomba = 0;
-const corrente = 0;
-const escudo = 0;
-const reset = 0;
+let zap = 0;
+let coringa = 0;
+let bomba = 0;
+let corrente = 0;
+let escudo = 0;
+let reset = 0;
+let baralhoEfeito = [zap, coringa, bomba, corrente, escudo, reset];
+let baralhoFull = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  zap,
+  coringa,
+  bomba,
+  corrente,
+  escudo,
+  reset,
+];
+let cartaComun = 0;
 // Baralho Fim
 
 // Mecanica do jogo
 function turnoUm() {
   let baralhoComun = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  let baralhoEfeito = [zap, coringa, bomba, corrente, escudo, reset];
   if (turnos == 1) {
-    console.log("Function esta funcionando");
     jogadorUm.maoJogadorUm = [
       baralhoComun[Math.floor(Math.random() * baralhoComun.length)],
       baralhoEfeito[Math.floor(Math.random() * baralhoEfeito.length)],
@@ -38,9 +56,29 @@ function turnoUm() {
       baralhoComun[Math.floor(Math.random() * baralhoComun.length)],
       baralhoEfeito[Math.floor(Math.random() * baralhoEfeito.length)],
     ];
+    turnos++;
   }
 }
 
+/*
+  Criar um Botão, que funcione como um contador, para que cada jogador click quando terminar a jogada
+  quando o contador for 2 execute a função para aumentar o numero de turnos, distribuir as cartasComuns na mesa
+  e nas mão dos jogadores.
+  
+function distribuirCartas() {
+  if (finalizarVez == 2) {
+    for (let i = 1; i < turnos; i++) {
+      turnos++;
+      cartaComun = [Math.floor(Math.random() * baralhoComun.length)];
+      jogadorUm.maoJogadorUm = [Math.floor(Math.random() * baralhoFull.length)];
+      jogadorDois.maoJogadorDois = [
+        Math.floor(Math.random() * baralhoFull.length),
+      ];
+    }
+  }
+}
+setInterval(distribuirCartas(), 1000);
+*/
 function pontuacao() {
   if (jogadorUm.pontos == 23 && jogadorDois.pontos == 23) {
     return console.log("Empate");
@@ -50,5 +88,5 @@ function pontuacao() {
     return console.log("Jogador Um Ganhou");
   }
 }
-pontuacao();
+setInterval(pontuacao(), 1000);
 // Mecanica do jogo Fim
